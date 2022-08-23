f33aac41acc908816a2a632205ddb75a167e2151
libin <civet148@126.com>
2022-08-24 07:53:10 +0800

    jsonrpc package upgrade to v0.2.1

--- dapp.go
+++ dapp.go
@@ -1,17 +1,27 @@
 package walletconnect
 
-//import "github.com/civet148/jsonrpc"
+import (
+	"github.com/civet148/jsonrpc"
+	"github.com/civet148/log"
+)
 
 type DApp struct {
 	wc string
+	uri *WalletConnectURI
+	ws *jsonrpc.WebSocketClient
 }
 
 // NewDApp creates a new DApp with the given wc uri
 func NewDApp(wc string) *DApp {
-
+    uri, err := ParseWC(wc)
+    if err != nil {
+    	log.Errorf(err.Error())
+    	return nil
+	}
 	return &DApp{
 		wc: wc,
-		//relay: jsonrpc.NewRelayClient(),
+		uri: uri,
+		ws: jsonrpc.NewWebSocketClient(uri.Bridge, nil),
 	}
 }
 
@@ -21,7 +31,8 @@ func (d *DApp) Publish() error {
 }
 
 
-func (d *DApp) Subscribe() error {
+func (d *DApp) Subscribe(topic string) error {
 
+	//d.ws.Subscribe()
 	return nil
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/civet148/walletconnect
 go 1.16
 
 require (
-	github.com/civet148/jsonrpc v0.2.0
+	github.com/civet148/jsonrpc v0.2.1
 	github.com/civet148/log v1.1.3
 	github.com/onsi/gomega v1.17.0 // indirect
 	github.com/qpliu/qrencode-go v0.0.0-20170225035013-ad8353b4581f
